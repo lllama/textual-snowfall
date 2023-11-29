27336807daeeeb27964cecf0db62ebc3e7320d02
Felix Ingram <f.ingram@gmail.com>
2023-11-29 14:22:34 +0000

    Make our snowflakes more unique

--- src/textual_snowfall/widgets.py
+++ src/textual_snowfall/widgets.py
@@ -1,15 +1,16 @@
 import random
-from itertools import takewhile, chain
+from itertools import chain, takewhile
 
 from rich.segment import Segment
 from rich.style import Style
-
 from textual.containers import Container
-from textual.strip import Strip
 from textual.geometry import Size
+from textual.strip import Strip
 
-SNOW = ["❅", "❆", "❄"]
-class FlakeColumn():
+SNOW = ["❄️", "❅", "❆", "❄"]
+
+
+class FlakeColumn:
     def __init__(self):
         self.speed = random.randrange(1, 10)
         self.tick_count = 0
@@ -27,7 +28,7 @@ class FlakeColumn():
             self.snow_fall.insert(0, " ")
             return
 
-        self.snow_fall.insert(0, "*")
+        self.snow_fall.insert(0, random.choice(SNOW))
         self.snow_fall = self.snow_fall[:200]
 
     def __getitem__(self, index):
@@ -37,14 +38,12 @@ class FlakeColumn():
             return " "
 
 
-
-
 def snow_machine():
     while True:
         yield FlakeColumn()
 
-class Snowfall(Container):
 
+class Snowfall(Container):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.set_interval(0.1, self.tick)
@@ -62,15 +61,15 @@ class Snowfall(Container):
         self.snowing = True
 
     def on_resize(self, event):
-        self.flakes = [col for (_, col) in takewhile(
-                lambda x: x[0] <= event.size.width,
-                enumerate(chain(self.flakes, snow_machine())))]
-
+        self.flakes = [
+            col
+            for (_, col) in takewhile(lambda x: x[0] <= event.size.width, enumerate(chain(self.flakes, snow_machine())))
+        ]
 
     def render_line(self, y):
         # if self.flakes:
         #     return Strip([Segment(f"{len(self.flakes)=}"), Segment(f"{self.flakes[0].snow_fall=}")])
         # return Strip([Segment(f"{len(self.flakes)=}")])
         # if y == 0:
-            # return Strip([Segment(f"{self.flakes=}")])
+        # return Strip([Segment(f"{self.flakes=}")])
         return Strip([Segment(column[y], Style(bold=True)) for column in self.flakes])
